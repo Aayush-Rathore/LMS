bf3e339ffe35cc590e43f58315365fd7d14e22c6
Aayush Rathore <aayu.r.2003@gmail.com>
2024-08-08 16:43:15 +0530

    ✅ utilities added

---
+++ back-end/src/database/models/student.model.ts
@@ -0,0 +1,72 @@
+import { Document, model, Schema } from "mongoose";
+import { hash } from "bcrypt";
+
+
+interface IStudent extends Document {
+    fullName: string,
+    dob: Date,
+    address: string,
+    profileUrl: string,
+    gender: string,
+    number: string,
+    email: string,
+    password?: string,
+}
+
+const StudentSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+
+    dob: {
+        type: Date,
+    },
+
+    address: {
+        type: String,
+    },
+
+    profileUrl: {
+        type: String,
+    },
+
+    gender: {
+        type: String,
+        enum: ["male", "female", "other"],
+        required: true
+
+    },
+
+    number: {
+        type: String,
+        required: true,
+        index: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        index: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        select: false
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+StudentSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await hash(this.password, 9);
+    next();
+});
+
+export const student = model<IStudent>("users", StudentSchema);
---
+++ back-end/src/validations/auth.validations.ts
@@ -0,0 +1,12 @@
+import BaseValidations from "./base.validations";
+import { z } from 'zod'
+
+class AuthValidations extends BaseValidations {
+    public SignUp = z.object({
+        fullName: this.fullName,
+        gender: this.gender,
+        number: this.number,
+        email: this.email,
+        password: this.password,
+    }).strict();
+}
